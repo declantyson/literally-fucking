30c3d855b7b94f53d2667b58bd95ddab7532a280
declantyson <declantyson@hotmail.co.uk>
2017-10-09 12:00:36 +0100

    Literally.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Literally -> Fucking
+
+### Why?
+
+The word literally is often used incorrectly or unnecessarily. So let's replace it with a word to add more literally emphasis.
---
+++ lf.js
@@ -0,0 +1,11 @@
+/*
+ *
+ *  literally-fucking
+ *  Declan Tyson
+ *  v0.0.1
+ *  09/10/2017
+ *
+ */
+
+var html = document.querySelector('html');
+html.innerHTML = html.innerHTML.replace(/literally/gi, '<strong>FUCKING</strong>');
